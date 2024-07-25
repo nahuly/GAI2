156d6784a3d097f89d18d4e9a19014604513f8a6
zoe.lee93(이나연)/kakaogames <zoe.lee93@kakaocorp.com>
2024-07-25 11:26:33 +0900

    cv2 수정

--- cartoon.py
+++ cartoon.py
@@ -1,40 +1,41 @@
-import streamlit as st
+import os
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from PIL import Image
-import io
+from skimage import transform
+import streamlit as st
 
-# Load image function
+# load image
 
 
-def li(img):
-    img = np.array(img.convert('RGB'))
-    img = img.astype(np.float32)/127.5 - 1
+def li(p):
+    img = Image.open(p)
+    img = np.array(img).astype(np.float32) / 127.5 - 1
     img = np.expand_dims(img, 0)
     img = tf.convert_to_tensor(img)
     return img
 
-# Preprocess image function
+# preprocess image
 
 
 def pi(img, td=224):
     shp = tf.cast(tf.shape(img)[1:-1], tf.float32)
     sd = min(shp)
-    scl = td/sd
-    nhp = tf.cast(shp*scl, tf.int32)
+    scl = td / sd
+    nhp = tf.cast(shp * scl, tf.int32)
     img = tf.image.resize(img, nhp)
     img = tf.image.resize_with_crop_or_pad(img, td, td)
     return img
 
-# Cartoon function
 
+def cartoon(img_p):
+    # loading image
+    si = li(img_p)
 
-def cartoon(img):
-    si = li(img)
     psi = pi(si, td=512)
+    psi.shape
 
-    # Model dataflow
+    # model dataflow
     m = 'cartoon_model.tflite'
     i = tf.lite.Interpreter(model_path=m)
     ind = i.get_input_details()
@@ -44,23 +45,32 @@ def cartoon(img):
 
     r = i.tensor(i.get_output_details()[0]['index'])()
 
-    # Post process the model output
-    o = (np.squeeze(r)+1.0)*127.5
+    # post process the model output
+    o = (np.squeeze(r) + 1.0) * 127.5
     o = np.clip(o, 0, 255).astype(np.uint8)
     o = Image.fromarray(o)
+    o = o.convert('RGB')
 
     return o
 
 
 # Streamlit app
-st.title('사진을 만화체로 변환하기')
+st.title('Cartoonify Your Image')
 
-uploaded_file = st.file_uploader("사진을 선택하세요", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader(
+    "Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='업로드된 이미지', use_column_width=True)
+    # Save the uploaded file to a temporary location
+    temp_file_path = os.path.join("temp", uploaded_file.name)
+    with open(temp_file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
 
-    if st.button('만화체로 변환'):
-        cartoon_image = cartoon(image)
-        st.image(cartoon_image, caption='만화체로 변환된 이미지', use_column_width=True)
+    # Display the input image
+    st.image(temp_file_path, caption='Input Image', use_column_width=True)
+
+    # Process the image
+    output_image = cartoon(temp_file_path)
+
+    # Display the output image
+    st.image(output_image, caption='Cartoonified Image', use_column_width=True)
