1acaa9ce8807198ba2146b344c1504e1310f026c
zoe.lee93(이나연)/kakaogames <zoe.lee93@kakaocorp.com>
2024-09-23 12:20:00 +0900

    test

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ def main():
     if 'results' not in st.session_state:
         st.session_state.results = None
 
+    st.write(data)
+    st.write(data['name'])
+
     # 질문 및 선택지 설정
     questions = {
         "당신이 선호하는 신장은?": data.loc['신장'].unique().tolist(),
@@ -34,39 +37,39 @@ def main():
         "어떤 색상의 정령을 선호하시나요?": data.loc['캐릭터 색상'].unique().tolist()
     }
 
-    # 질문 표시 및 응답 수집
-    for question, options in questions.items():
-        response = st.selectbox(question, options, key=question)
-        st.session_state.responses[question] = response
+    # # 질문 표시 및 응답 수집
+    # for question, options in questions.items():
+    #     response = st.selectbox(question, options, key=question)
+    #     st.session_state.responses[question] = response
 
-    # 결과 계산 버튼
-    if st.button("결과 보기"):
-        scores = {spirit: 0 for spirit in data.columns}
+    # # 결과 계산 버튼
+    # if st.button("결과 보기"):
+    #     scores = {spirit: 0 for spirit in data.columns}
 
-        for question, response in st.session_state.responses.items():
-            for spirit in data.columns:
-                if question == "당신이 선호하는 신장은?":
-                    if response == data.loc['신장', spirit]:
-                        scores[spirit] += 1
-                elif question == "어떤 취미를 가진 정령을 좋아하시나요?":
-                    if response == data.loc['취미', spirit]:
-                        scores[spirit] += 1
-                elif question == "어떤 특기를 가진 정령을 선호하시나요?":
-                    if response == data.loc['특기', spirit]:
-                        scores[spirit] += 1
-                elif question == "정령이 좋아하는 것 중 당신의 취향과 맞는 것은?":
-                    if response == data.loc['좋아하는 것', spirit]:
-                        scores[spirit] += 1
-                elif question == "어떤 색상의 정령을 선호하시나요?":
-                    if response.lower() == data.loc['캐릭터 색상', spirit].lower():
-                        scores[spirit] += 1
+    #     for question, response in st.session_state.responses.items():
+    #         for spirit in data.columns:
+    #             if question == "당신이 선호하는 신장은?":
+    #                 if response == data.loc['신장', spirit]:
+    #                     scores[spirit] += 1
+    #             elif question == "어떤 취미를 가진 정령을 좋아하시나요?":
+    #                 if response == data.loc['취미', spirit]:
+    #                     scores[spirit] += 1
+    #             elif question == "어떤 특기를 가진 정령을 선호하시나요?":
+    #                 if response == data.loc['특기', spirit]:
+    #                     scores[spirit] += 1
+    #             elif question == "정령이 좋아하는 것 중 당신의 취향과 맞는 것은?":
+    #                 if response == data.loc['좋아하는 것', spirit]:
+    #                     scores[spirit] += 1
+    #             elif question == "어떤 색상의 정령을 선호하시나요?":
+    #                 if response.lower() == data.loc['캐릭터 색상', spirit].lower():
+    #                     scores[spirit] += 1
 
-        # 점수에 따라 정령 정렬
-        st.session_state.results = sorted(
-            scores.items(), key=lambda x: x[1], reverse=True)
+    #     # 점수에 따라 정령 정렬
+    #     st.session_state.results = sorted(
+    #         scores.items(), key=lambda x: x[1], reverse=True)
 
-        # 결과 페이지로 이동
-        st.experimental_rerun()
+    #     # 결과 페이지로 이동
+    #     st.experimental_rerun()
 
 
 if __name__ == "__main__":
