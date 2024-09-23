11b34d8a997b6135929e02366083bdb298ee6b2b
zoe.lee93(이나연)/kakaogames <zoe.lee93@kakaocorp.com>
2024-09-23 14:40:25 +0900

    test

--- eversoul.py
+++ eversoul.py
@@ -28,8 +28,8 @@ questions = {
     "어떤 색상의 정령을 선호하시나요?": data['캐릭터 색상'].unique().tolist()
 }
 
-# st.write(data)
-# st.write(data['name'])
+st.write(data)
+st.write(data['name'])
 # st.write(data['소속'].unique().tolist())
 # st.write(data.loc[data['name'] == '제이드', '신장'].values[0])
 
