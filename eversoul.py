e841723a5d29a7aa931065d02110a4d4dafeede6
zoe.lee93(이나연)/kakaogames <zoe.lee93@kakaocorp.com>
2024-09-26 09:53:25 +0900

    test

--- eversoul.py
+++ eversoul.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-from PIL import Image
 
 # 데이터 로드
 
@@ -14,18 +13,8 @@ def load_data():
     return spirits_data_cleaned.drop(spirits_data_cleaned.index[0])
     # return spirits_data
 
-# 이미지 로드 함수
-
-
-def load_spirit_image(spirit_name):
-    try:
-        return Image.open(f"{spirit_name}.png")
-    except FileNotFoundError:
-        return None
-
 
 data = load_data()
-# st.write(data)
 
 # 웹사이트 제목
 st.title("당신의 최애 정령 찾기")
@@ -78,34 +67,13 @@ if st.button("결과 보기"):
 
     # 점수에 따라 정령 정렬
     ranked_spirits = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-    st.write(ranked_spirits)
-
-    # # 결과 표시
-    # st.subheader("당신의 최애 정령 순위:")
-    # for rank, (spirit, score) in enumerate(ranked_spirits[:3], 1):
-    #     st.write(f"{rank}위: {spirit}")
-    #     st.write(f"소속: {data.loc[data['name'] == spirit, 'team'].values[0]}")
-    #     st.write(f"특기: {data.loc[data['name'] == spirit, '특기'].values[0]}")
-    #     st.write(f"취미: {data.loc[data['name'] == spirit, '취미'].values[0]}")
-    #     st.write("---")
+    # st.write(ranked_spirits)
 
     # 결과 표시
     st.subheader("당신의 최애 정령 순위:")
     for rank, (spirit, score) in enumerate(ranked_spirits[:3], 1):
-        col1, col2 = st.columns([1, 3])
-
-        # with col1:
-        #     image = load_spirit_image(spirit)
-        #     if image:
-        #         st.image(image, caption=spirit, use_column_width=True)
-        #     else:
-        #         st.write("이미지 없음")
-
-        with col2:
-            st.subheader(f"{rank}위: {spirit}")
-            st.write(
-                f"소속: {data.loc[data['name'] == spirit, '소속'].values[0]}")
-            st.write(f"특기: {data.loc[data['name'] == spirit, '특기'].values[0]}")
-            st.write(f"취미: {data.loc[data['name'] == spirit, '취미'].values[0]}")
-
+        st.write(f"{rank}위: {spirit}")
+        st.write(f"소속: {data.loc[data['name'] == spirit, '소속'].values[0]}")
+        st.write(f"특기: {data.loc[data['name'] == spirit, '특기'].values[0]}")
+        st.write(f"취미: {data.loc[data['name'] == spirit, '취미'].values[0]}")
         st.write("---")
