9e43bfb216348436053557ba37d2af57ee9d857c
zoe.lee93(이나연)/kakaogames <zoe.lee93@kakaocorp.com>
2024-12-17 18:52:24 +0900

    dd

--- extraction.py
+++ extraction.py
@@ -46,10 +46,11 @@ st.plotly_chart(fig)
 # 'Date' 컬럼에서 월만 추출
 df['Month'] = df['Date'].str[5:7]
 
-# 'Month'별로 'cnt' 합산
-month_stats = df.groupby('Month').agg(
-    cnt=('Month', 'size'),  # 행의 개수
-)
+print(df.head())
+# # 'Month'별로 'cnt' 합산
+# month_stats = df.groupby('Month').agg(
+#     cnt=('Month', 'size'),  # 행의 개수
+# )
 
-# 결과를 Streamlit의 bar_chart로 시각화
-st.bar_chart(month_stats.set_index('Month')['cnt'])
+# # 결과를 Streamlit의 bar_chart로 시각화
+# st.bar_chart(month_stats.set_index('Month')['cnt'])
