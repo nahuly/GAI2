252dadba7a18001d7b300874d24fcdd4c285f746
zoe.lee93(이나연)/kakaogames <zoe.lee93@kakaocorp.com>
2024-12-17 17:37:24 +0900

    add

--- extraction.py
+++ extraction.py
@@ -7,29 +7,18 @@ import plotly.express as px
 # Write directly to the app
 st.title("추출 :balloon:")
 
-cnt = pd.read_csv('test.csv')
+df = pd.read_csv('test.csv')
 
 
 st.header("추출 개수")
-st.metric(label='총 추출 개수', value=len(cnt))
-st.metric(label='총 추출 개수', value=len(cnt), delta='-50%')
+st.metric(label='총 추출 개수', value=len(df))
+st.metric(label='총 추출 개수', value=len(df), delta='-50%')
 
 # -----------------------------------------------------------
-df = pd.DataFrame(
-    {
-        "a": range(1, 3),  # 리스트로 값 전달
-        "b": range(1, 3),  # 리스트로 값 전달
-    }
-)
 
-st.header("sql 그래프")
-
-df = pd.DataFrame(
-    {
-        "a": range(1, 3),  # 리스트로 값 전달
-        "b": range(2, 4),  # 리스트로 값 전달
-    }
+id_stats = df.groupby('AgitID').agg(
+    cnt=('AgitID', 'size'),  # 행의 개수
 )
 
 # 데이터프레임을 bar_chart로 시각화
-st.bar_chart(df, x="a", y="b")
+st.bar_chart(df, x="AgitID", y="cnt")
